e94a4d6698d30af8b3507466406c1e8bdca5392f
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-21 19:35:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  string = string.toUpperCase
+  string = string.toUpperCase()
   console.log(string)
   
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
-  else if (string === string.toLowerCase) {
+  else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
-  else if (string === string.toUpperCase) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
   
